5c38b8659b0240e9ee81d7d5f7fda01b3cba7ebd
Salja <beleko2009@web.de>
2011-08-28 12:49:08 +0200

    [DEV 0029] Fix Sheeped/Feared Targets Bugging. Thanks Zataron
    Signed-off-by: Salja <beleko2009@web.de>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7085,7 +7085,8 @@ bool Unit::SelectHostileTarget()
 
     if (target)
     {
-        if (!hasUnitState(UNIT_STAT_STUNNED | UNIT_STAT_DIED))
+        if (!hasUnitState(UNIT_STAT_STUNNED | UNIT_STAT_DIED) &&
+            !HasAuraType(SPELL_AURA_MOD_FEAR) && !HasAuraType(SPELL_AURA_MOD_CONFUSE))
         {
             SetInFront(target);
             ((Creature*)this)->AI()->AttackStart(target);
